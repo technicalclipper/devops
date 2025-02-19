74eab9ad99f2dab8c70072c4836e57568ebaacfa
technicalclipper <technicalclipper@gmail.com>
2025-02-19 12:12:15 +0530

    added user state for login

--- frontend/app/src/login.jsx
+++ frontend/app/src/login.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 export function Login(){
-    return(
+    const [user, setUser] = useState({ username: "", password: "" });
+
+    function handleUserInput(e) {
+        const { name, value } = e.target;
+        setUser((prevUser) => ({
+            ...prevUser,
+            [name]: value
+        }));
+    }
+
+    return (
         <div className='loginbg'>
             <div className='logintitle'>BUILD YOUR PC</div>
             
             <div className='logincenter'>
                 <div className='logincontainer'>
-                    <input className="logininput" type="text" name="username" placeholder='USERNAME'></input>
-                    <input className="logininput" type="password" name="username" placeholder='PASSWORD'></input>
+                    <input
+                        className="logininput"
+                        type="text"
+                        name="username"
+                        value={user.username}
+                        onChange={handleUserInput}
+                        placeholder='USERNAME'
+                    />
+                    <input
+                        className="logininput"
+                        type="password"
+                        name="password"
+                        value={user.password}
+                        onChange={handleUserInput}
+                        placeholder='PASSWORD'
+                    />
                     <div className='forgotpassword'>FORGOT PASSWORD?</div>
                     <div className='innerlogin'>
                         <div className='login'>LOGIN</div>
-                        <img className='or' src="images/orline.png"/>
-                        <img className='googlelogin' src="images/google_button.png"/>
+                        <img className='or' src="images/orline.png" alt="or" />
+                        <img className='googlelogin' src="images/google_button.png" alt="google login" />
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
